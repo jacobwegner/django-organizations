7ccf0427a4e418e14ed422f6f2998cc57f90e1fc
Jake Wegner <jake@mclemoreauction.com>
2013-04-03 13:44:05 -0500

    Pinned Django to 1.4.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'Framework :: Django',
     ],
     install_requires=[
-        'Django>=1.4',
+        'Django>=1.4, <1.5',
         'django-extensions>=0.9',
     ],
     #test_suite='tests.runtests.runtests',
